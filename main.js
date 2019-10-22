a236f312415ae2e40a4026e6815fd8a51fd88617
Nikola Djordjevic <Nikola.dj.87@gmail.com>
2019-10-22 03:24:36 +0200

    s

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.open-menu-icon').click(function () {
+    $('.main-nav-outer').addClass('open-menu')
+})
+
+$('.menu-close-icon').click(function () {
+    $('.main-nav-outer').removeClass('open-menu')
+})
